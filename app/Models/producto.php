778b600fffdffcead954968bab396a3fcd6a99c2
unknown <jhoeldebrayquispe@gmail.com>
2023-11-22 14:37:05 -0400

    producto me jodio

--- app/Models/producto.php
+++ app/Models/producto.php
@@ -4,18 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Menu;
+use App\Models\Categoria;
 
-class producto extends Model
+class Producto extends Model
 {
     use HasFactory;
     protected $table = 'productos';
-    protected $fillable = [
-        'categoria_id',
-        'nombre',
-        'precio' ,
-        'imagen' ,
-        'stock' ,
-        'descripcion' ,
-        'disponibilida' 
-    ];
+
+
+    public function menu()
+    {
+        return $this->belongsToMany(Menu::class, 'menu_platos');
+    }
+
+    public function categoria()
+    {
+        return $this->belongsTo(categoria::class);
+    }
 }
