b46d836d679c49b95e5b964ba759183da2fa970b
unknown <jhoeldebrayquispe@gmail.com>
2023-11-22 14:17:53 -0400

    crud producto5

--- app/Models/producto.php
+++ app/Models/producto.php
@@ -5,17 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class producto extends Model
+class Producto extends Model
 {
     use HasFactory;
-    protected $table = 'productos';
-    protected $fillable = [
-        'categoria_id',
-        'nombre',
-        'precio' ,
-        'imagen' ,
-        'stock' ,
-        'descripcion' ,
-        'disponibilida' 
-    ];
-}
+    }
